9e9860e84e0f4f87e30249b7ec08eb912a6eee90
fdfdd12345628 <fdfdd12345628@gmail.com>
2019-12-16 22:28:56 +0800

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,5 +1,6 @@
 import json, math
 import numpy as np
+from PIL import Image
 
 
 def construct(came_from, end, start):
@@ -7,7 +8,7 @@ def construct(came_from, end, start):
     current = int(came_from.flat[end])
     while True:
         try:
-            if path_list[len(path_list)-1]==current:
+            if path_list[len(path_list) - 1] == current:
                 return path_list
         except IndexError:
             pass
@@ -21,7 +22,7 @@ def construct(came_from, end, start):
 
 
 def h(data, came_from, current_node, target_node, start_node):
-    if target_node==None:
+    if target_node == None:
         return 500
     start_x = current_node // data.shape[1]
     start_y = current_node % data.shape[1]
@@ -36,9 +37,11 @@ def d(data, came_from, current_node, target_node, start_node):
     start_y = current_node % data.shape[1]
     end_x = target_node // data.shape[1]
     end_y = target_node % data.shape[1]
-    result = -data.flat[target_node] * 1
+    result = -(data.flat[target_node] * 1)
+    print(data.flat[target_node])
+    print(data.flat[current_node])
     result += math.sqrt((start_x - end_x) ** 2 + (start_y - end_y) ** 2)
-    path=construct(came_from, current_node, start_node)
+    path = construct(came_from, current_node, start_node)
     # child_node=0
     for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:  # Adjacent squares
         try:
@@ -55,9 +58,15 @@ def d(data, came_from, current_node, target_node, start_node):
         except:
             pass
         if chile_node in path:
-            result-=0.03
+            pass
+            # result -= 0.03
 
     return result
+
+def pre(data):
+    data=data[1:-1, 1:-1]
+    return data
+    pass
 
 
 # def h(data, came_from, data_start, target_end):
@@ -65,6 +74,9 @@ def d(data, came_from, current_node, target_node, start_node):
 
 
 def a_star(data, start=None, end=None, blocks=0):
+    data=pre(data)
+    if start is None:
+        start=np.argmax(data)
     g_score = np.ones(data.shape) * 1000
     f_score = np.ones(data.shape) * 1000
     open_set = {start: 0}
@@ -74,7 +86,7 @@ def a_star(data, start=None, end=None, blocks=0):
     came_from_list = np.zeros(data.shape, dtype=np.int)
     walked = {}
     if start is None:
-        start=np.argmax(data)
+        start = np.argmax(data)
     while len(open_set) != 0:
         # current_min=f_score.max
         current = np.argmax(f_score)
@@ -84,7 +96,7 @@ def a_star(data, start=None, end=None, blocks=0):
         if current == np.argmax(f_score):
             break
         # current = np.argmin(f_score)
-        if end!=None:
+        if end != None:
             if current == end:
                 path = construct(came_from_list, end, start)
 
@@ -92,13 +104,13 @@ def a_star(data, start=None, end=None, blocks=0):
         else:
             # with np.argmin(g_score) as end:
             # temp_end=np.argmin(g_score)
-            temp_end=0
+            temp_end = 0
             for i in open_set:
-                if g_score.flat[i]<g_score.flat[temp_end]:
-                    temp_end=i
-            path=construct(came_from_list, temp_end, start)
-            total=len(path)
-            if total>=blocks:
+                if g_score.flat[i] < g_score.flat[temp_end]:
+                    temp_end = i
+            path = construct(came_from_list, temp_end, start)
+            total = len(path)
+            if total >= blocks:
                 return path
 
         del (open_set[current])
@@ -108,12 +120,14 @@ def a_star(data, start=None, end=None, blocks=0):
                 # chile_node = data.flat[current + data.shape[1] * new_position[0] + new_position[1]]
                 # chile_node = data[current // data.shape[1] + new_position[0] + 1][
                 #   current % data.shape[1] + new_position[1]]
-                if current//data.shape[1]+new_position[0]==data.shape[0] or current//data.shape[1]+new_position[0]<0:
+                if current // data.shape[1] + new_position[0] == data.shape[0] or current // data.shape[1] + \
+                        new_position[0] < 0:
                     continue
-                if current%data.shape[1]+new_position[0]==data.shape[1] or current%data.shape[1]+new_position[1]<0:
+                if current % data.shape[1] + new_position[0] == data.shape[1] or current % data.shape[1] + new_position[
+                    1] < 0:
                     continue
                 chile_node = current + data.shape[1] * new_position[0] + new_position[1]
-                if chile_node>= data.size:
+                if chile_node >= data.size:
                     continue
             except IndexError as e:
                 continue
@@ -132,19 +146,25 @@ def a_star(data, start=None, end=None, blocks=0):
 
 
 if __name__ == '__main__':
+    # test=np.array(range(100))
+    # test=np.reshape(test, (10, 10))
+    # test=pre(test)
+    # print(test)
     with open('區塊坑洞挖掘與施工紀錄_1000公尺.json', 'r+', encoding='utf8') as file:
-        raw = file.read()
-        raw = raw.replace('\'', '"')
-        json_data = json.loads(raw)
+        raw = Image.open('map_pred1.bmp')
+        test=np.array(raw)
+        test=test[:,:,0]
+        # json_data = json.loads(raw)
     # print(json_data)
-    test = np.array(json_data['data'][15]['channel_hole'])
+    # test = np.array(json_data['data'][15]['channel_hole'])
     '''for i in range(10)[::-1]:
         path = a_star(test, np.argmax(test), blocks=i)
         if path is not None:
             break'''
-    path = a_star(test, blocks=4)
+    path = a_star(test, blocks=15)
     print(path)
-    # path_array = np.zeros(test.shape)
-    # for i in path:
-    #     path_array.flat[i] = 1
+    test=test[1:-1, 1:-1]
+    path_array = np.zeros(test.shape)
+    for i in path:
+        path_array.flat[i] = 1
     pass
