e524d110fc84ceea018b4a85a7d09783e07362aa
fdfdd12345628 <fdfdd12345628@gmail.com>
2020-02-06 15:09:46 +0800

    Update admin.py

--- city/app/admin.py
+++ city/app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import hole,weather
+from .models import hole, weather, examination, modelResult
+
 # Register your models here.
 admin.site.register(hole)
 admin.site.register(weather)
+admin.site.register(examination)
+admin.site.register(modelResult)
